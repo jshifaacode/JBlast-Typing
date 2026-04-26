24a036b386e1519149ff538256f9ee2d290f3a08
Jayshifa Banyuwana <jayshifabanyuwana@gmail.com>
2026-04-26 18:37:53 +0700

    Update words.js

--- js/words.js
+++ js/words.js
@@ -1,66 +1,54 @@
 const Words = (() => {
   const easy = [
-     'sus','cegil','fomo','gokil','gas','mbg','izin','hts','relate',
-      'cringe','sus','mabar','clingy','baper','mager','bucin','gabut',
-      'dawg','spill','slay','vibe','plenger','kocak', 'bro',
-      'galau','kepo','alay','asik','mantap','crush','kicau','gamon'
+    'sus','cegil','fomo','gokil','gas','mbg','izin','hts','relate',
+    'cringe','mabar','clingy','baper','mager','bucin','gue',
+    'dawg','spill','ceo','vibe','wibu','kocak','bro',
+    'galau','kepo','alay','asik','damn','crush','kicau','gamon'
   ];
-
   const medium = [
-    'prikitiw','fineshyt','kalcer','yapping','asyiap',
-    'validasi','ekspektasi','realita','kelas king','dejavu',
-    'bolang','friendlly','gimmick','furab','wibu',
-    'respect','santuy','excited','salting','recehan',
-    'miris','ghosting','bahlil','jokowi','esempe',
-    ];
-
-  const hard = [  
-    'overthinking','mie ayam','spotify',
-    'waspada','understand','miscommunication',
-    'misinterpretasi','Sangat gokil','selfdiagnosis',
-    'quarterlifecrisis','overcomplicated','underestimated',
-    'hyperfixation','instantgratification','delusional',
+    'bolang','fineshyt','kalcer','yapping','asyiap',
+    'validasi','ekspektasi','realita','kelasking','dejavu',
+    'prikitiw','friendlly','gimmick','furab','plenger',
+    'respect','santuy','excited','salting','ngakak',
+    'miris','ghosting','bahlil','jokowi','naksir'
+  ]; 
+  const hard = [
+    'overthinking','mieayam','spotify',
+    'jyujyur','understand','artistic',
+    'dinosaurus','sangat gokil','selfrewards',
+    'intimidasi','aplikasi','kesepian',
+    'alamakkk','awokawokawok','halusinasi'
   ];
-
   const boss = [
-    'LAU SAPE MPRUY',
-    'KASIH PAHAM BOS',
-    'KATAKAN WHEN YAH',
-    'SAJA ADA ADA',
-    'DIT TOLONGIN DIT',
-    'KASIH PAHAM BRAY',
-    'HIDUP JOKOWI',
-    'HEY ANTEK ASING'
+    'lausapempruy','kasihpahambos','katakanwhenyah',
+    'sajaadaada','dittolongindit','kasihpahambray',
+    'hidupjokowi','heyantekasing'
   ];
 
-  const glitch = (word) => {
-    const chars = word.split('');
-    const glitchChars = '@#$%&*!?^~';
-    return chars.map((c, i) => {
-      if (c === ' ') return c;
-      return Math.random() < 0.3 ? glitchChars[Math.floor(Math.random() * glitchChars.length)] : c;
-    }).join('');
-  };
+  
+  function makeQueue(arr) {
+    let q = [];
+    return () => {
+      if (q.length === 0) q = [...arr].sort(() => Math.random() - 0.5);
+      return q.shift();
+    };
+  }
 
-  const hideChars = (word) => {
-    return word.split('').map((c, i) => {
-      if (c === ' ') return c;
-      return i % 3 === 0 ? '_' : c;
-    }).join('');
-  };
+  const nextEasy   = makeQueue(easy);
+  const nextMedium = makeQueue(medium);
+  const nextHard   = makeQueue(hard);
+  const nextBoss   = makeQueue(boss);
 
   return {
-    getEasy: () => easy[Math.floor(Math.random() * easy.length)],
-    getMedium: () => medium[Math.floor(Math.random() * medium.length)],
-    getHard: () => hard[Math.floor(Math.random() * hard.length)],
-    getBoss: () => boss[Math.floor(Math.random() * boss.length)],
-    getByWave: (wave) => {
+    getEasy:   () => nextEasy(),
+    getMedium: () => nextMedium(),
+    getHard:   () => nextHard(),
+    getBoss:   () => nextBoss(),
+    getByWave(wave) {
       if (wave <= 2) return Words.getEasy();
       if (wave <= 5) return Words.getMedium();
       return Words.getHard();
     },
-    glitch,
-    hideChars,
     getAll: () => [...easy, ...medium, ...hard]
   };
 })();
