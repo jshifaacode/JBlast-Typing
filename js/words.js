6af7c50e24be573981bda36770cc19599f7794f0
Jayshifa Banyuwana <jayshifabanyuwana@gmail.com>
2026-05-04 10:52:13 +0700

    Update words.js

--- js/words.js
+++ js/words.js
@@ -1,31 +1,92 @@
 const Words = (() => {
   const easy = [
-    'sus','cegil','fomo','gokil','gas','mbg','izin','hts','relate',
-    'cringe','mabar','clingy','baper','mager','bucin','gue',
-    'dawg','spill','ceo','vibe','wibu','kocak','bro',
-    'galau','kepo','alay','asik','damn','crush','kicau','gamon'
+    "sus",
+    "cegil",
+    "fomo",
+    "gokil",
+    "gas",
+    "mbg",
+    "izin",
+    "hts",
+    "relate",
+    "cringe",
+    "mabar",
+    "clingy",
+    "baper",
+    "mager",
+    "bucin",
+    "gue",
+    "dawg",
+    "spill",
+    "ceo",
+    "vibe",
+    "wibu",
+    "kocak",
+    "bro",
+    "galau",
+    "kepo",
+    "alay",
+    "asik",
+    "damn",
+    "crush",
+    "kicau",
+    "gamon",
   ];
   const medium = [
-    'bolang','fineshyt','kalcer','yapping','asyiap',
-    'validasi','ekspektasi','realita','kelasking','dejavu',
-    'prikitiw','friendlly','gimmick','furab','plenger',
-    'respect','santuy','excited','salting','ngakak',
-    'miris','ghosting','bahlil','jokowi','naksir'
-  ]; 
+    "bolang",
+    "fineshyt",
+    "kalcer",
+    "yapping",
+    "asyiap",
+    "validasi",
+    "egiluy",
+    "realita",
+    "kelasking",
+    "dejavu",
+    "prikitiw",
+    "friendlly",
+    "gimmick",
+    "furab",
+    "plenger",
+    "respect",
+    "santuy",
+    "excited",
+    "salting",
+    "ngakak",
+    "miris",
+    "ghosting",
+    "bahlil",
+    "jokowi",
+    "naksir",
+  ];
   const hard = [
-    'overthinking','mieayam','spotify',
-    'jyujyur','understand','artistic',
-    'dinosaurus','sangat gokil','selfrewards',
-    'intimidasi','aplikasi','kesepian',
-    'alamakkk','awokawokawok','halusinasi'
+    "overthinking",
+    "mieayam",
+    "spotify",
+    "jyujyur",
+    "understand",
+    "artistic",
+    "dinosaurus",
+    "sangatgokil",
+    "selfrewards",
+    "intimidasi",
+    "aplikasi",
+    "kesepian",
+    "alamakkk",
+    "awokawokawok",
+    "halusinasi",
   ];
   const boss = [
-    'lausapempruy','kasihpahambos','katakanwhenyah',
-    'sajaadaada','dittolongindit','kasihpahambray',
-    'hidupjokowi','heyantekasing'
+    "lausapempruy",
+    "kasihpahambos",
+    "katakanwhenyah",
+    "tidakfantash",
+    "dittolongindit",
+    "kasihpahambray",
+    "hidupjokowi",
+    "heyantekasing",
   ];
 
-  
   function makeQueue(arr) {
     let q = [];
     return () => {
@@ -34,21 +95,21 @@ const Words = (() => {
     };
   }
 
-  const nextEasy   = makeQueue(easy);
+  const nextEasy = makeQueue(easy);
   const nextMedium = makeQueue(medium);
-  const nextHard   = makeQueue(hard);
-  const nextBoss   = makeQueue(boss);
+  const nextHard = makeQueue(hard);
+  const nextBoss = makeQueue(boss);
 
   return {
-    getEasy:   () => nextEasy(),
+    getEasy: () => nextEasy(),
     getMedium: () => nextMedium(),
-    getHard:   () => nextHard(),
-    getBoss:   () => nextBoss(),
+    getHard: () => nextHard(),
+    getBoss: () => nextBoss(),
     getByWave(wave) {
       if (wave <= 2) return Words.getEasy();
       if (wave <= 5) return Words.getMedium();
       return Words.getHard();
     },
-    getAll: () => [...easy, ...medium, ...hard]
+    getAll: () => [...easy, ...medium, ...hard],
   };
 })();
